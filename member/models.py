b6444f2add785eabf058c25af62d23ffe8d79123
sercanarici <sercanarici@hotmail.com>
2015-08-19 19:49:51 +0300

    models.py updated

--- member/models.py
+++ member/models.py
@@ -1,18 +1,46 @@
 from django.db import models
-
 # Create your models here.
 
 
 class Member(models.Model):
     username = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
     email = models.CharField(max_length=50, unique=True)
+    url = models.URLField()
+    twitter = models.URLField()
+    github = models.URLField()
+    facebook = models.URLField()
 
-    def __unicode__(self):
-        return self.username
+    def __print__(self):
+            return self.username
+
 
 class Ticket(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
-    release_date = models.DateField()
+    create_date = models.DateTimeField(auto_now_add=True)
     ticket_member = models.ForeignKey('Member', related_name="tickets")
+    is_open = models.BooleanField(default=True)
+    category = models.ForeignKey('TicketCategory', related_name='tickets')
+
+    def __print__(self):
+        return self.title
+
+
+class Answers(models.Model):
+    member = models.ForeignKey("Member", related_name="answers")
+    content = models.TextField()
+    ticket_id = models.ForeignKey("Ticket", related_name="answers")
+    create_date = models.DateTimeField(auto_now_add=True)
+
+    def __print__(self):
+        return self.member.username
+
+
+class TicketCategory(models.Model):
+    name = models.TextField(max_length=60)
+
+    def __print__(self):
+        return self.name
