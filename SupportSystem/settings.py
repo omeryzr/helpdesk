9a65d431f65141d0d0d61be64707acb8752b4a81
omeryzr <omeryzr_@hotmail.com>
2015-08-19 10:50:49 +0300

    create member app

--- SupportSystem/settings.py
+++ SupportSystem/settings.py
@@ -60,7 +60,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.debug'
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -101,3 +101,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+SITE_ID = 1
