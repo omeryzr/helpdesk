2a0640328c299cc9a41873e16dc1ea67110e892d
sercanarici <sercanarici@hotmail.com>
2015-08-21 02:46:51 +0300

    Create custom admin model for ticket and userdetail models

--- member/admin.py
+++ member/admin.py
@@ -3,7 +3,16 @@ from member.models import *
 # Register your models here.
 
 
-admin.site.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'category', 'create_date')
+    search_fields = ('title','content')
+
+
+class UserDetailAdmin(admin.ModelAdmin):
+    list_display = ('user', 'url')
+
+
+admin.site.register(Ticket, TicketAdmin)
 admin.site.register(TicketCategory)
 admin.site.register(Answer)
-admin.site.register(UserDetails)
+admin.site.register(UserDetails, UserDetailAdmin)
