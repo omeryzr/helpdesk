fa9ed346dffbd159e2678683bed5660097919a7e
omeryzr <omeryzr_@hotmail.com>
2015-08-21 14:00:18 +0300

    qw

--- member/migrations/0001_initial.py
+++ member/migrations/0001_initial.py
@@ -15,29 +15,15 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Answer',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
                 ('content', models.TextField()),
                 ('create_date', models.DateTimeField(auto_now_add=True)),
             ],
         ),
         migrations.CreateModel(
-            name='Member',
-            fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
-                ('name', models.CharField(max_length=50)),
-                ('surname', models.CharField(max_length=50)),
-                ('url', models.URLField()),
-                ('twitter', models.URLField(blank=True)),
-                ('github', models.URLField(blank=True)),
-                ('facebook', models.URLField(blank=True)),
-                ('photo', models.URLField(blank=True)),
-                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
-            ],
-        ),
-        migrations.CreateModel(
             name='Ticket',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
                 ('title', models.CharField(max_length=255)),
                 ('content', models.TextField()),
                 ('create_date', models.DateTimeField(auto_now_add=True)),
@@ -47,28 +33,48 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='TicketCategory',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
                 ('name', models.CharField(max_length=60)),
             ],
+            options={
+                'verbose_name': 'Ticket Category',
+                'verbose_name_plural': 'Ticket Categories',
+            },
+        ),
+        migrations.CreateModel(
+            name='UserDetails',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('url', models.URLField()),
+                ('twitter', models.URLField(blank=True)),
+                ('github', models.URLField(blank=True)),
+                ('facebook', models.URLField(blank=True)),
+                ('photo', models.URLField(blank=True)),
+                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'verbose_name': 'User Detail',
+                'verbose_name_plural': 'User Details',
+            },
         ),
         migrations.AddField(
             model_name='ticket',
             name='category',
-            field=models.ForeignKey(to='member.TicketCategory', related_name='tickets'),
+            field=models.ForeignKey(related_name='tickets', to='member.TicketCategory'),
         ),
         migrations.AddField(
             model_name='ticket',
-            name='ticket_member',
-            field=models.ForeignKey(to='member.Member', related_name='tickets'),
-        ),
-        migrations.AddField(
-            model_name='answer',
-            name='member',
-            field=models.ForeignKey(to='member.Member', related_name='answers'),
+            name='user',
+            field=models.ForeignKey(to=settings.AUTH_USER_MODEL),
         ),
         migrations.AddField(
             model_name='answer',
             name='ticket_id',
-            field=models.ForeignKey(to='member.Ticket', related_name='answers'),
+            field=models.ForeignKey(related_name='answers', to='member.Ticket'),
+        ),
+        migrations.AddField(
+            model_name='answer',
+            name='user',
+            field=models.ForeignKey(to=settings.AUTH_USER_MODEL),
         ),
     ]
